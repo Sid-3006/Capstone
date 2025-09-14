3f79213c2ace9dde46241adc68def45c6158f1c2
sid-3006 <siddharthsithasan30@gmail.com>
2025-09-14 12:25:14 +0530

    v1.0

---
+++ src/Components/Title.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div className='flex items-center justify-center h-screen text-3xl font-semibold'>Hey there, Sid here 👋👋</div>
+  )
+}
+
+export default Title
