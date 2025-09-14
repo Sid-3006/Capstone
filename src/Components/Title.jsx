cc69c679ca21ed660a5c63f7937cdb804fae6466
sid-3006 <siddharthsithasan30@gmail.com>
2025-09-14 12:34:34 +0530

    h6 tag added

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Title = () => {
   return (
+    <>
     <div className='flex items-center justify-center h-screen text-3xl font-semibold'>Hey there, Sid here 👋👋</div>
+    <h6>Beginning my journey as a developer</h6>
+   </>
   )
 }
 
